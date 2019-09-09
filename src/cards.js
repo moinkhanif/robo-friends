3883ad03dd379bfdac57906dd2cb4a6b2a9954e7
Moin Khan <kmoin786@gmail.com>
2019-09-09 13:12:46 +0530

    added dynamic robo list

---
+++ src/cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const cards = ({id,name,username,email}) => {
+    return(
+        <div  className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt="robot pic"/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+export default cards;
