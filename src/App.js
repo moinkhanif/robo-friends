e85698ea10d1f241d874438dc26437ab08ecd2fa
Moin Khan <kmoin786@gmail.com>
2019-09-11 20:53:58 +0530

    added filter to search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './cardlist';
 import {robots} from './robots';
 import SearchBox from './searchbox';
-const App = () => {
-    return(
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots = {robots} />
-        </div>
-        
-    );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase())
+        }
+
+        )
+        return(
+
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots = {filteredRobots} />
+            </div>
+            );
+        }
+    
 }
 export default App;
